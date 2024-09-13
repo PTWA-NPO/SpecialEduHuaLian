9c9b132a29114b43afe8d0018aa63faa943bfd81
Justin <chenyouduan@gmail.com>
2024-09-13 23:29:06 +0800

    fix: file path error

--- src/views/index.vue
+++ src/views/index.vue
@@ -120,7 +120,7 @@ export default {
   left: 0;
   width: 100%;
   height: 100%;
-  background-image: url(../assets/images/pics/cover_info.PNG);
+  background-image: url(../assets/images/pics/cover_info.png);
   background-repeat: no-repeat;
   background-position: center center;
   background-size: cover;
